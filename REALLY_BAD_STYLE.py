ce0271e7d280ad17e84a3c4bc8f3145f1c5ad6c7
DRMPN <mr.gorden.smit@gmail.com>
2023-11-23 23:09:31 +0300

    test now

---
+++ REALLY_BAD_STYLE.py
@@ -0,0 +1,8 @@
+def function():
+  'wrong'
+  pass
+def extra_long__long__long__long__long__long__long__long__long__long__long__long__long__long__long_function():
+    "wrong spacing before"
+    pass
+def long_inside_function():
+   return "long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_long_"
